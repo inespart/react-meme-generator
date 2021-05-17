ebc023450f8297dc857de2fb94e1f61518b091e7
Ines Part <inespart@Iness-MacBook-Air.local>
2021-05-17 11:05:34 +0200

    Add dropdown selector for image style and include download functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './style.css';
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
   const [loading, setLoading] = useState();
   const [error, setError] = useState();
   const [topText, setTopText] = useState('');
   const [bottomText, setBottomText] = useState('');
-  // here comes image style variable
-  const [previewMeme, setPreviewMeme] = useState();
+  const [styleImage, setStyleImage] = useState();
   const [customUrl, setCustomUrl] = useState(
     'https://api.memegen.link/images/ams/Hello/my friends.png',
   );
@@ -18,25 +17,74 @@ function App() {
 
   // Fetch template
   useEffect(() => {
-    fetch(url)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw response;
-      })
-      .then((data) => {
-        setData(data);
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-        setError(error);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchData();
   }, []);
+
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then((response) => {
+  //       if (response.ok) {
+  //         return response.json();
+  //       }
+  //       throw response;
+  //     })
+  //     .then((data) => {
+  //       setData(data);
+  //       // console.log(data);
+  //       const styleNames = data.map((item) => {
+  //         return item.id;
+  //       });
+  //       console.log('CL from useEffect');
+  //       console.log(styleNames);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error fetching data:', error);
+  //       setError(error);
+  //     })
+  //     .finally(() => {
+  //       setLoading(false);
+  //     });
+  // }, []);
+
+  // Functionality to download custom meme
+
+  function forceDownload(blob, filename) {
+    // Create an invisible anchor element
+    const anchor = document.createElement('a');
+    anchor.style.display = 'none';
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.setAttribute('download', filename);
+    document.body.appendChild(anchor);
+
+    // Trigger the download by simulating click
+    anchor.click();
+
+    // Clean up
+    window.URL.revokeObjectURL(anchor.href);
+    document.body.removeChild(anchor);
+  }
+  function downloadResource(URL, filename) {
+    // If no filename is set, use filename from URL
+    if (!filename) filename = URL.match(/\/([^/#?]+)[^/]*$/)[1];
+    fetch(URL, {
+      headers: new Headers({
+        Origin: window.location.origin,
+      }),
+      mode: 'cors',
+    })
+      .then((response) => response.blob())
+      .then((blob) => forceDownload(blob, filename))
+      .catch((e) => console.error(e));
+  }
 
   if (loading) return 'Loading...';
   if (error) return 'Error!';
@@ -74,24 +122,47 @@ function App() {
 
         <div className="inputArea">
           <label htmlFor="styleImage">Image style: </label>
-          <input type="text" id="styleImage" />
+          <select
+            type="text"
+            id="styleImage"
+            placeholder="buzz"
+            onChange={(e) => {
+              setStyleImage(e.currentTarget.value);
+            }}
+          >
+            <option>Please select</option>
+
+            {data.map((item) => (
+              <option value={item.id} key={item.id}>
+                {item.name}
+              </option>
+            ))}
+          </select>
         </div>
 
         <div>
           <button
             onClick={() => {
-              setCustomUrl(
-                `https://api.memegen.link/images/buzz/` +
-                  topText +
-                  `/` +
-                  bottomText +
-                  `.jpg`,
-              );
+              !styleImage
+                ? setCustomUrl(
+                    `https://api.memegen.link/images/ams/${topText}/${bottomText}.jpg`,
+                  )
+                : setCustomUrl(
+                    `https://api.memegen.link/images/${styleImage}/${topText}/${bottomText}.jpg`,
+                  );
             }}
           >
             Preview meme
           </button>
-          <button>Download meme</button>
+          <button
+            onClick={() => {
+              downloadResource(
+                `https://api.memegen.link/images/${styleImage}/${topText}/${bottomText}.jpg`,
+              );
+            }}
+          >
+            Download meme
+          </button>
         </div>
 
         <div>
